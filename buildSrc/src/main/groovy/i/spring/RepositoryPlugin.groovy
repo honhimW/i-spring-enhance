25451ab2fe5ba24a2ee70d46a739f6505fd19fed
honhimW <honhimw@outlook.com>
2025-11-28 14:31:16 +0800

    fix: RepositoryPlugin

--- buildSrc/src/main/groovy/i/spring/RepositoryPlugin.groovy
+++ buildSrc/src/main/groovy/i/spring/RepositoryPlugin.groovy
@@ -10,7 +10,7 @@ class RepositoryPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.plugins.apply MavenPublishPlugin
-        if (!project.rootProject.file('buildSrc/maven-repository.gradle').exists()) {
+        if (project.rootProject.file('buildSrc/maven-repository.gradle').exists()) {
             project.apply from: project.rootProject.file('buildSrc/maven-repository.gradle')
         } else {
             project.repositories.mavenLocal()
